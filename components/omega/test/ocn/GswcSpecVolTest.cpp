82747a776fca115a96236e39be64513e83adf323
Alice Barthel <abarthel@lanl.gov>
2025-01-06 14:07:48 -0600

    added the GSW-C vs published value of spec vol

--- components/omega/test/ocn/GswcSpecVolTest.cpp
+++ components/omega/test/ocn/GswcSpecVolTest.cpp
@@ -13,23 +13,22 @@
 #include "DataTypes.h"
 #include "Decomp.h"
 #include "Dimension.h"
-#include "Field.h"
-#include "Halo.h"
-#include "HorzMesh.h"
 #include "IO.h"
 #include "Logging.h"
 #include "MachEnv.h"
+#include "OceanTestCommon.h"
 #include "OmegaKokkos.h"
 #include "TimeStepper.h"
 #include "mpi.h"
 
 #include <iostream>
-#include </home/ac.abarthel/E3SMv3/code/add-eos/components/omega/external/GSW-C/gswteos-10.h>
+#include <gswteos-10.h>
 
 using namespace OMEGA;
 
 // misc. variables for testing
-const Real RefReal = 3.0;
+const Real DeltaRefReal = 0.0009776149797;
+const Real VolRefReal = 0.0009732819628;
 
 //------------------------------------------------------------------------------
 // The initialization routine for Teos10 testing. It calls various
@@ -52,118 +51,57 @@ I4 initTeosTest() {
    Config("Omega");
    Err = Config::readAll("omega.yml");
    if (Err != 0) {
-      LOG_ERROR("Tracers: Error reading config file");
-      return Err;
-   }
-
-   // Initialize the default time stepper
-   Err = TimeStepper::init1();
-   if (Err != 0) {
-      LOG_ERROR("Tracers: error initializing default time stepper");
+      LOG_ERROR("Teos: Error reading config file");
       return Err;
    }
 
    // Initialize the IO system
    Err = IO::init(DefComm);
    if (Err != 0) {
-      LOG_ERROR("Tracers: error initializing parallel IO");
-      return Err;
-   }
-
-   // Create the default decomposition (initializes the decomposition)
-   Err = Decomp::init();
-   if (Err != 0) {
-      LOG_ERROR("Tracers: error initializing default decomposition");
-      return Err;
-   }
-
-   // Initialize the default halo
-   Err = Halo::init();
-   if (Err != 0) {
-      LOG_ERROR("Tracers: error initializing default halo");
-      return Err;
-   }
-
-   // Initialize the default mesh
-   Err = HorzMesh::init();
-   if (Err != 0) {
-      LOG_ERROR("Tracers: error initializing default mesh");
+      LOG_ERROR("Teos: error initializing parallel IO");
       return Err;
    }
 
    return 0;
 }
 
+
+int test_specvol_value() {
+   int Err = 0;
+   const Real RTol = 1e-10;
+   double Sa = 30.;
+   double Ct = 10.;
+   double P = 1000.;
+   
+   double SpecVol = gsw_specvol(Sa, Ct, P);
+   LOG_INFO("GswcTeosTest: produced SpecVol from GSW-C module");
+   LOG_INFO("Value of SpecVol: {}", SpecVol);
+   bool Check = isApprox(SpecVol, VolRefReal, RTol);
+   if (!Check) {
+      Err++;
+      LOG_ERROR("GswcTeosTest: SpecVol isApprox FAIL, expected {}, got {}",
+                VolRefReal, SpecVol);
+   }
+   if (Err == 0) {
+      LOG_INFO("GswcTeosTest: check PASS");
+   }
+   return Err;
+}
+
+
 //------------------------------------------------------------------------------
 // The test driver for Teos10 library testing -> this tests calls the library 
-// and reads in the TEOS10 constants.
+// and compares the specific volume to the published value
 //
 int main(int argc, char *argv[]) {
 
    int RetVal = 0;
-   I4 Err;
-   I4 count = 0;
 
-   // Initialize the global MPI environment
    MPI_Init(&argc, &argv);
-   Kokkos::initialize();
-   {
+   Kokkos::initialize(argc, argv);
 
-      // Call initialization routine
-      Err = initTeosTest();
-      if (Err != 0)
-         LOG_ERROR("Tracers: Error initializing");
+   RetVal += test_specvol_value();
 
-      // Get MPI vars if needed
-      MachEnv *DefEnv = MachEnv::getDefault();
-      MPI_Comm Comm   = DefEnv->getComm();
-      I4 MyTask       = DefEnv->getMyTask();
-      I4 NumTasks     = DefEnv->getNumTasks();
-      bool IsMaster   = DefEnv->isMasterTask();
-
-      HorzMesh *DefHorzMesh       = HorzMesh::getDefault();
-      Decomp *DefDecomp           = Decomp::getDefault();
-      Halo *DefHalo               = Halo::getDefault();
-      TimeStepper *DefTimeStepper = TimeStepper::getDefault();
-
-      
-      std::cout << "testing the teos test" << std::endl;
-      // GSW_TEOS10_CONSTANTS;
-      double print_specvol_test() {
-        double sa = 30.;
-        double ct = 10.;
-	double p = 1000.;
-        double delta = gsw_specvol(sa, ct, p);
-
-        return delta;
-      }
-
-      // initialize Tracers infrastructure
-      Err = Tracers::init();
-      if (Err != 0) {
-         RetVal += 1;
-         LOG_ERROR("Tracers: initialzation FAIL");
-      }
-
-      I4 NTracers    = Tracers::getNumTracers();
-      I4 NCellsOwned = DefHorzMesh->NCellsOwned;
-      I4 NCellsSize  = DefHorzMesh->NCellsSize;
-      I4 NVertLevels = DefHorzMesh->NVertLevels;
-      I4 NTimeLevels = DefTimeStepper->getNTimeLevels();
-
-
-      Tracers::clear();
-      TimeStepper::clear();
-      HorzMesh::clear();
-      Decomp::clear();
-      MachEnv::removeAll();
-      FieldGroup::clear();
-      Field::clear();
-      Dimension::clear();
-
-      if (RetVal == 0)
-         LOG_INFO("Teos lib call: Successful completion");
-   }
    Kokkos::finalize();
    MPI_Finalize();
 
