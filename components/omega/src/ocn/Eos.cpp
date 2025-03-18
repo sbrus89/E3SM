a0f3379dab18f33387971fe08df3de7b939c2de3
Alice Barthel <abarthel@lanl.gov>
2025-03-18 14:54:57 -0500

    capitalization change to resolve read pbl

--- components/omega/src/ocn/Eos.cpp
+++ components/omega/src/ocn/Eos.cpp
@@ -1,5 +1,5 @@
-#include "DataTypes.h"
 #include "Eos.h"
+#include "DataTypes.h"
 #include "HorzMesh.h"
 
 namespace OMEGA {
@@ -12,9 +12,9 @@ TEOS10Poly75t::TEOS10Poly75t() {}
 LinearEOS::LinearEOS() {}
 
 Eos::Eos(const std::string &Name, ///< [in] Name for eos object
-             const HorzMesh *Mesh,    ///< [in] Horizontal mesh
-             int NVertLevels         ///< [in] Number of vertical levels
-){
+         const HorzMesh *Mesh,    ///< [in] Horizontal mesh
+         int NVertLevels          ///< [in] Number of vertical levels
+) {
    SpecVol = Array2DReal("SpecVol", Mesh->NCellsSize, NVertLevels);
    SpecVolDisplaced =
        Array2DReal("SpecVolDisplaced", Mesh->NCellsSize, NVertLevels);
@@ -24,18 +24,17 @@ Eos::Eos(const std::string &Name, ///< [in] Name for eos object
    NChunks   = NVertLevels / VecLength;
    // is this where I should add the EosType
 
-} //end constructor
+} // end constructor
 
 // Initialize the SpecVol Arrays. Assumes Hormesh is initialized
-int Eos::init(){
+int Eos::init() {
 
-   int Err = 0;
+   int Err               = 0;
    HorzMesh *DefHorzMesh = HorzMesh::getDefault();
-   I4 NVertLevels = DefHorzMesh->NVertLevels;
+   I4 NVertLevels        = DefHorzMesh->NVertLevels;
 
    // Create default eos
-   Eos::DefaultEos =
-      create("Default", DefHorzMesh, NVertLevels);
+   Eos::DefaultEos = create("Default", DefHorzMesh, NVertLevels);
 
    // Get EosConfig group
    Config *OmegaConfig = Config::getOmegaConfig();
@@ -53,9 +52,9 @@ int Eos::init(){
       return Err;
    }
 
-   if (EosTypeStr == "Linear"){
+   if (EosTypeStr == "Linear") {
       DefaultEos->eosChoice = EosType::Linear;
-   } else if (EosTypeStr == "teos10"){
+   } else if (EosTypeStr == "teos10") {
       DefaultEos->eosChoice = EosType::TEOS10Poly75t;
    } else {
       LOG_CRITICAL("Eos: Unknown EosType requested");
@@ -63,14 +62,13 @@ int Eos::init(){
       return Err;
    }
 
-
-   Err = EosConfig.get("LineardRhodT", DefaultEos->lineardRhodT);
+   Err = EosConfig.get("LinearDRhoDT", DefaultEos->lineardRhodT);
    if (Err != 0) {
       LOG_CRITICAL("Eos: linear dRhodT not found in "
                    "EosConfig");
       return Err;
    }
-   Err = EosConfig.get("LineardRhodS", DefaultEos->lineardRhodS);
+   Err = EosConfig.get("LinearDRhoDS", DefaultEos->lineardRhodS);
    if (Err != 0) {
       LOG_CRITICAL("Eos: linear dRhodS not found in "
                    "EosConfig");
@@ -78,12 +76,12 @@ int Eos::init(){
    }
 
    LOG_INFO("set default values {}, {}", DefaultEos->lineardRhodT,
-		   DefaultEos->lineardRhodS);
+            DefaultEos->lineardRhodS);
    return Err;
 } // end init
 
 void Eos::computeSpecVol(Array2DReal &SpecVol,
-		         const Array2DReal &ConservativeTemperature,
+                         const Array2DReal &ConservativeTemperature,
                          const Array2DReal &AbsoluteSalinity,
                          const Array2DReal &Pressure) {
    OMEGA_SCOPE(LocSpecVol, SpecVol);
@@ -91,48 +89,33 @@ void Eos::computeSpecVol(Array2DReal &SpecVol,
    // OMEGA_SCOPE(LocComputeSpecVolTeos10, computeSpecVolTeos10);
    // deepCopy(, 0); Is this needed? Why would it be?
 
-    if (eosChoice == EosType::Linear){
-        parallelFor(
-       "eos-linear", {NCellsAll, NChunks},
-       KOKKOS_LAMBDA(int ICell, int KChunk) {
-          computeSpecVolLinear(LocSpecVol, ICell, KChunk,
-          ConservativeTemperature,
-          AbsoluteSalinity,
-          Pressure)
-          ;
-       });
-    }
-    else if (eosChoice == EosType::TEOS10Poly75t){
-         parallelFor(
-       "eos-teos10", {NCellsAll, NChunks},
-       KOKKOS_LAMBDA(int ICell, int KChunk) {
-          computeSpecVolTEOS10Poly75t(LocSpecVol,
-			  ICell, KChunk,
-                          ConservativeTemperature,
-                          AbsoluteSalinity,
-                          Pressure)
-                          ;
-       });
-    }
-
+   if (eosChoice == EosType::Linear) {
+      parallelFor(
+          "eos-linear", {NCellsAll, NChunks},
+          KOKKOS_LAMBDA(int ICell, int KChunk) {
+             computeSpecVolLinear(LocSpecVol, ICell, KChunk,
+                                  ConservativeTemperature, AbsoluteSalinity,
+                                  Pressure);
+          });
+   } else if (eosChoice == EosType::TEOS10Poly75t) {
+      parallelFor(
+          "eos-teos10", {NCellsAll, NChunks},
+          KOKKOS_LAMBDA(int ICell, int KChunk) {
+             computeSpecVolTEOS10Poly75t(LocSpecVol, ICell, KChunk,
+                                         ConservativeTemperature,
+                                         AbsoluteSalinity, Pressure);
+          });
+   }
 }
 
-
-
-void Eos::computeSpecVolLinear(Array2DReal SpecVol,
-                         int ICell, int KChunk,
-			 const Array2DReal &ConservativeTemperature,
-                         const Array2DReal &AbsoluteSalinity,
-                         const Array2DReal &Pressure) {
-}
-void Eos::computeSpecVolTEOS10Poly75t(Array2DReal SpecVol,
-                         int ICell, int KChunk,
-			 const Array2DReal &ConservativeTemperature,
-                         const Array2DReal &AbsoluteSalinity,
-                         const Array2DReal &Pressure) {
-}
-
-
+void Eos::computeSpecVolLinear(Array2DReal SpecVol, int ICell, int KChunk,
+                               const Array2DReal &ConservativeTemperature,
+                               const Array2DReal &AbsoluteSalinity,
+                               const Array2DReal &Pressure) {}
+void Eos::computeSpecVolTEOS10Poly75t(
+    Array2DReal SpecVol, int ICell, int KChunk,
+    const Array2DReal &ConservativeTemperature,
+    const Array2DReal &AbsoluteSalinity, const Array2DReal &Pressure) {}
 
 //------------------------------------------------------------------------------
 // Destroys the eos class
@@ -142,26 +125,17 @@ Eos::~Eos() {
 
 } // end destructor
 
-
 //------------------------------------------------------------------------------
 // Removes all eos instances before exit
 void Eos::clear() { AllEos.clear(); } // end clear
 
 //------------------------------------------------------------------------------
 // Removes eos from list by name
-void Eos::erase(const std::string &Name) {
-
-   AllEos.erase(Name);
-
-} // end erase
+void Eos::erase(const std::string &Name) { AllEos.erase(Name); } // end erase
 
 //------------------------------------------------------------------------------
 // Get default eos
-Eos *Eos::getDefault() {
-
-   return Eos::DefaultEos;
-
-} // end get default
+Eos *Eos::getDefault() { return Eos::DefaultEos; } // end get default
 
 //------------------------------------------------------------------------------
 // Get eos by name
@@ -173,8 +147,7 @@ Eos *Eos::get(const std::string &Name ///< [in] Name of eos
    if (it != AllEos.end()) {
       return it->second.get();
    } else {
-      LOG_ERROR(
-          "Eos::get: Attempt to retrieve non-existent eos:");
+      LOG_ERROR("Eos::get: Attempt to retrieve non-existent eos:");
       LOG_ERROR("{} has not been defined or has been removed", Name);
       return nullptr;
    }
